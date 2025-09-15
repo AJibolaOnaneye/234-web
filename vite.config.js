2e0bf839a614c5c2f8318c92cf3776dd524f6d81
AJibolaOnaneye <62557029+AJibolaOnaneye@users.noreply.github.com>
2025-09-15 03:47:39 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/234odds'
+  base: '/234odds/'
 })
